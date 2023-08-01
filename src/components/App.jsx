52b179987350d3e10998cf52db57de1e00ba62a8
Oleh Tsap <olehtsap07@gmail.com>
2023-08-01 19:33:05 +0300

    почали розбирати бібліотеку styled-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { GlobalStyle } from "./Global.styled";
+import { Alert } from "./Alert/Alert";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <>
+    <GlobalStyle/>
+    <Alert type="error" />
+    <Alert type="success" />
+    <Alert type="warning" />    
+  </>
+
   );
 };
